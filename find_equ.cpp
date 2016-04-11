9ba4c28db6fe481566d395c1a7af019e6bb2b114
ranjan1990 <ranjan.rbc@gmail.com>
2016-04-11 16:36:22 +0530

    .row() changed to .col()

--- find_equ.cpp
+++ find_equ.cpp
@@ -21,55 +21,78 @@ class MyFunction
 {
    public:
        MyFunction() {
-          data.load("traj.txt");
-
+        data::Load("traj.txt",data1);
+        cout<<" "<<data1.n_cols<<"   "<<data1.n_rows<<endl;
        }
+
        size_t NumFunctions() const { return 20; }
        arma::mat GetInitialPoint() const { return arma::mat("0.555;0.2203;-0.5269;6.1637"); }
        double Evaluate(const arma::mat& coordinates, const size_t i) const ;
        void Gradient(const arma::mat& coordinates,const size_t i,arma::mat& gradient) const ;
-    private:
-      arma::mat data;
+   private:
+      arma::mat data1;
 
 };
 
 
 double MyFunction::Evaluate(const arma::mat& coordinates, const size_t i) const
 {
-      arma::mat Ri=data.row(i);
-      Ri.insert_cols(3,1);
-      Ri[3]=1;
-      double val=arma::det(coordinates.t()*Ri.t());
+      arma::mat Ri=data1.col(i);
+
+      //Ri.insert_rows(3,1);
+     // Ri[3]=1;
+      double val=0;
+      for(size_t j=0;j<3;j++)
+      {
+          val=val+ Ri[j]*coordinates[j];
+
+      }
+      val=val+ coordinates[3] ; 
+
       return(val*val);
 }
 
 void MyFunction::Gradient(const arma::mat& coordinates, const size_t i, arma::mat& gradient) const
 {
   gradient.zeros(4);
-  arma::mat Ri=data.row(i);
-  Ri.insert_cols(3,1);
-  Ri[3]=1;
-  double val=arma::det(coordinates.t()*Ri.t());
+  arma::mat Ri=data1.col(i);
+      double val=0;
+      for(size_t j=0;j<3;j++)
+      {
+          val=val+ Ri[j]*coordinates[j];
+
+      }
+      val=val+ coordinates[3] ; 
+
   gradient[i]=2*val*Ri[i];
 
 }
 
 main()
 {
-  MyFunction f;
- 
-  arma::mat gradient; 
   
-  SGD<MyFunction> s(f, 0.000003, 5000, 1e-14, false);
-  arma::mat coordinates = f.GetInitialPoint();
-  double result1 = s.Optimize(coordinates);
- /* 
+  
+  MyFunction f=MyFunction();
+  arma::mat gradient; 
+ // arma::mat coordinates1 = f.GetInitialPoint();
+  arma::mat coordinates2 = f.GetInitialPoint();
+
+//  ParallelSGD<MyFunction> s1(f, 0.00003, 10000000, 1e-9);
+  SGD<MyFunction> s2(f, 0.00003, 10000, 1e-9, false);
+
+  //double result1 = s1.Optimize(coordinates1);
+  double result2 = s2.Optimize(coordinates2);
+
+   
   for(int i=0;i<50;i++)
   {
 
-    cout<<f.Evaluate(coordinates,i)<<endl;
+    //cout<<f.Evaluate(coordinates1,i)<<endl;
+    cout<<f.Evaluate(coordinates2,i)<<endl;
   }
-*/
+   
+//    cout<<coordinates1<<endl;
+    cout<<coordinates2<<endl;
 
 
 }
